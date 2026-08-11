5f3aea24144b7eabd00cdbaa75f2b46f2c79e34e
choi2704 <takismjy@naver.com>
2026-08-11 15:36:15 +0900

    Add files via upload

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Supabase 프로젝트 정보를 넣어주세요.
 // Project Settings → API 에서 확인할 수 있습니다.
 window.KD_CONFIG = {
-  SUPABASE_URL: "https://xkzrsavlfoxvogdytprg.supabase.co/rest/v1/",
+  SUPABASE_URL: "https://xkzrsavlfoxvogdytprg.supabase.co",
   SUPABASE_ANON_KEY: "sb_publishable_qBHuTjxTuCwYONjn1C00jQ_-UKt8IEw"
 };
